da79ead5dbf82c2a0f5224426c6066fed2b03fad
abdelrahman <eng.abdo.com@gmail.com>
2023-06-29 17:44:28 +0300

    add init file

---
+++ init.php
@@ -0,0 +1,33 @@
+<?php
+
+require 'Applecation'.DIRECTORY_SEPARATOR.'config.php';
+
+require 'Applecation'.DIRECTORY_SEPARATOR.'Routes'.DIRECTORY_SEPARATOR.'URLGenerator.php';
+
+require 'Applecation'.DIRECTORY_SEPARATOR.'Controllers'.DIRECTORY_SEPARATOR.'ProductController.php';
+
+require 'Applecation'.DIRECTORY_SEPARATOR.'Liberary'.DIRECTORY_SEPARATOR.'URLHandle.php';
+
+require 'Applecation'.DIRECTORY_SEPARATOR.'Liberary'.DIRECTORY_SEPARATOR.'View.php';
+
+require 'Applecation'.DIRECTORY_SEPARATOR.'Liberary'.DIRECTORY_SEPARATOR.'Product'.DIRECTORY_SEPARATOR.'Product.php';
+
+require 'Applecation'.DIRECTORY_SEPARATOR.'Liberary'.DIRECTORY_SEPARATOR.'Validation'.DIRECTORY_SEPARATOR.'Products'.DIRECTORY_SEPARATOR.'ProductValidation.php';
+
+require 'Applecation'.DIRECTORY_SEPARATOR.'Liberary'.DIRECTORY_SEPARATOR.'Validation'.DIRECTORY_SEPARATOR.'Products'.DIRECTORY_SEPARATOR.'DVDProductValidation.php';
+
+require 'Applecation'.DIRECTORY_SEPARATOR.'Liberary'.DIRECTORY_SEPARATOR.'Validation'.DIRECTORY_SEPARATOR.'Products'.DIRECTORY_SEPARATOR.'BookProductValidation.php';
+
+require 'Applecation'.DIRECTORY_SEPARATOR.'Liberary'.DIRECTORY_SEPARATOR.'Validation'.DIRECTORY_SEPARATOR.'Products'.DIRECTORY_SEPARATOR.'FurnitureProductValidation.php';
+
+require 'Applecation'.DIRECTORY_SEPARATOR.'Liberary'.DIRECTORY_SEPARATOR.'Product'.DIRECTORY_SEPARATOR.'DVDProduct.php';
+
+require 'Applecation'.DIRECTORY_SEPARATOR.'Liberary'.DIRECTORY_SEPARATOR.'Product'.DIRECTORY_SEPARATOR.'BookProduct.php';
+
+require 'Applecation'.DIRECTORY_SEPARATOR.'Liberary'.DIRECTORY_SEPARATOR.'Product'.DIRECTORY_SEPARATOR.'FurnitureProduct.php';
+
+require 'Applecation'.DIRECTORY_SEPARATOR.'Models'.DIRECTORY_SEPARATOR.'Model.php';
+
+require 'Applecation'.DIRECTORY_SEPARATOR.'Models'.DIRECTORY_SEPARATOR.'Product.php';
+
+require_once 'Applecation'.DIRECTORY_SEPARATOR.'Liberary'.DIRECTORY_SEPARATOR.'DBConnection'.DIRECTORY_SEPARATOR.'DBConnection.php';
